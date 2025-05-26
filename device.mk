5b7b54de31276ce7f3ef3316ccb4e72e2fb7f085
ALBINsRAJ2025 <albinsraj2025@gmail.com>
2025-05-26 21:01:11 +0530

    device.mk : Enabled Virtual AB Flag

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Virtual A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
